4006e6d07377795440ae61d691d9a3183df2196a
Yaroslav Halchenko <debian@onerussian.com>
2013-02-12 13:15:11 -0500

    repack -- prefix all our messages with I: to easily parse output
    +fixing path to GEMS

--- debian/repack.sh
+++ debian/repack.sh
@@ -16,11 +16,11 @@ wdir=$(mktemp -d)
 # process input
 fs_snapshot="$1"
 if [ -d "$fs_snapshot" ]; then
-	echo "Copy/filter sources into $wdir"
+	echo "I: Copy/filter sources into $wdir"
 	tar $tar_excludes -cf- "$fs_snapshot" | tar -xf- -C "$wdir"
 	dname=$(basename $fs_snapshot)
 else
-	echo "Unpack sources into $wdir"
+	echo "I: Unpack sources into $wdir"
 	tar $tar_excludes -xf "$curdir/$fs_snapshot"  -C "$wdir"
 	dname=dev
 fi
@@ -30,7 +30,7 @@ cd $wdir
 # Assure that it is called dev for consistency
 [ "$dname" = "dev" ] && dname="+dev" || { mv $dname dev; dname=""; }
 
-echo "Determine version"
+echo "I: Determine version"
 # Use STABLE_VER_NUM to deduce the base version
 uversion_raw=$(sed -n -e '/STABLE_VER_NUM/s,.*="v\(.*\)",\1,gp' dev/distribution/build_release_type.csh)
 LC_ALL=C lastmod=$(find dev -type f -exec ls -og --time-style=long-iso {} \; |sort --key 4 |tail -n1 | cut -d ' ' -f 4,4 | tr -d "-")
@@ -41,9 +41,9 @@ LC_ALL=C lastmod=$(find dev -type f -exec ls -og --time-style=long-iso {} \; |so
 # higher than the one in stable release
 uversion="${uversion_raw}${dname}+cvs${lastmod}"
 
-echo "Got version: ${uversion}"
+echo "I: Got version: ${uversion}"
 
-echo "Repackaging"
+echo "I: Repackaging"
 mkdir -p $data_dir
 mkdir -p ${nonfree_dir}/scripts
 # major hunk of data
@@ -62,7 +62,7 @@ mv dev/talairach_avi/3T18yoSchwartzReactN32_as_orig* \
 find dev/fsfast/docs/ -name '*.pdf' -delete
 find dev/ -name 'callgrind*' -delete
 find dev/ -name '*.bak' -delete
-find GEMS/ -name '*.o' -delete
+find dev/ -name '*.o' -delete
 rm dev/INSTALL
 rm dev/config/*
 rm dev/qdec/qdec-boad-wink.wnk
@@ -80,7 +80,7 @@ mv $(grep -il CorTech dev/scripts/*) ${nonfree_dir}/scripts
 # but at least they are legal to distribute, so let's keep them
 
 
-echo "Compute checksums"
+echo "I: Compute checksums"
 # compute checksums for data and store them in the sources
 find $data_dir -type f -exec md5sum {} \; > dev/freesurfer-data.md5sums
 sed -i -e "s,$data_dir,.," dev/freesurfer-data.md5sums
@@ -90,17 +90,17 @@ mv freesurfer-data freesurfer-data-${uversion}
 mv dev freesurfer-${uversion}
 mv freesurfer-nonfree freesurfer-nonfree-${uversion}
 
-echo "Build orig tarballs"
+echo "I: Build orig tarballs"
 tar -czf freesurfer_${uversion}.orig.tar.gz freesurfer-${uversion}
 tar -czf freesurfer-data_${uversion}.orig.tar.gz freesurfer-data-${uversion}
 tar -czf freesurfer-nonfree_${uversion}.orig.tar.gz freesurfer-nonfree-${uversion}
 
-echo "Move tarballs to final destination"
+echo "I: Move tarballs to final destination"
 mv freesurfer*.orig.tar.gz $curdir
 
-echo "Clean working directory"
+echo "I: Clean working directory"
 rm -rf $wdir
 
-echo "Done"
+echo "I: Done"
 
 
